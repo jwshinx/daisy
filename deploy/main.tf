5a876644abf475fb8ef978998dd9ec1cfb871dc0
Joel Shin <joel@fluxxlabs.com>
2021-02-12 23:42:20 -0800

    runs apply fine; but lb still 503 and target draining loop

--- deploy/main.tf
+++ deploy/main.tf
@@ -111,6 +111,14 @@ resource "aws_ecs_service" "weather_service" {
   launch_type     = "FARGATE"
   desired_count   = 3 # Setting the number of containers we want deployed to 3
 
+  load_balancer {
+    target_group_arn = aws_lb_target_group.target_group.arn # Referencing our target group
+    # container_name   = aws_ecs_task_definition.weather_task.family
+    # try again
+    container_name   = "weather_app"
+    container_port   = 3000 # Specifying the container port
+  }
+
   network_configuration {
     subnets = [
       aws_default_subnet.default_subnet_a.id,
@@ -137,4 +145,62 @@ resource "aws_default_subnet" "default_subnet_b" {
 
 resource "aws_default_subnet" "default_subnet_c" {
   availability_zone = "us-east-1c"
+}
+
+#####################################################################
+# load balancer
+#####################################################################
+resource "aws_alb" "application_load_balancer" {
+  name               = "weather-lb" # Naming our load balancer
+  load_balancer_type = "application"
+  subnets = [ # Referencing the default subnets
+    aws_default_subnet.default_subnet_a.id,
+    aws_default_subnet.default_subnet_b.id,
+    aws_default_subnet.default_subnet_c.id
+  ]
+  # Referencing the security group
+  security_groups = [aws_security_group.load_balancer_security_group.id]
+}
+
+# Creating a security group for the load balancer:
+resource "aws_security_group" "load_balancer_security_group" {
+  ingress {
+    # from_port   = 80 # Allowing traffic in from port 80
+    # to_port     = 80
+    # protocol    = "tcp"
+    # cidr_blocks = ["0.0.0.0/0"] # Allowing traffic in from all sources
+    from_port   = 0             # Allowing any incoming port
+    to_port     = 0             # Allowing any outgoing port
+    protocol    = "-1"          # Allowing any outgoing protocol 
+    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
+  }
+
+  egress {
+    from_port   = 0             # Allowing any incoming port
+    to_port     = 0             # Allowing any outgoing port
+    protocol    = "-1"          # Allowing any outgoing protocol 
+    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
+  }
+}
+
+resource "aws_lb_target_group" "target_group" {
+  name        = "target-group"
+  port        = 80
+  protocol    = "HTTP"
+  target_type = "ip"
+  vpc_id      = aws_default_vpc.default_vpc.id # Referencing the default VPC
+  health_check {
+    matcher = "200,301,302"
+    path    = "/"
+  }
+}
+
+resource "aws_lb_listener" "listener" {
+  load_balancer_arn = aws_alb.application_load_balancer.arn # Referencing our load balancer
+  port              = "80"
+  protocol          = "HTTP"
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.target_group.arn # Referencing our tagrte group
+  }
 }
