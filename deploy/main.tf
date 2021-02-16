1a6a89c6ef6b41f8d784632740f2ea6f61748c79
Joel Shin <joel@fluxxlabs.com>
2021-02-16 15:05:06 -0800

    worked: locked down lb security group ingress traffic to port 80

--- deploy/main.tf
+++ deploy/main.tf
@@ -186,15 +186,22 @@ resource "aws_security_group" "load_balancer_security_group" {
   vpc_id      = aws_default_vpc.default_vpc.id
 
   ingress {
-    # from_port   = 80 # Allowing traffic in from port 80
-    # to_port     = 80
-    # protocol    = "tcp"
-    # cidr_blocks = ["0.0.0.0/0"] # Allowing traffic in from all sources
-    from_port   = 0             # Allowing any incoming port
-    to_port     = 0             # Allowing any outgoing port
-    protocol    = "-1"          # Allowing any outgoing protocol 
-    cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
+    protocol    = "tcp"
+    from_port   = 80
+    to_port     = 80
+    cidr_blocks = ["0.0.0.0/0"]
   }
+
+  # ingress {
+  #   # from_port   = 80 # Allowing traffic in from port 80
+  #   # to_port     = 80
+  #   # protocol    = "tcp"
+  #   # cidr_blocks = ["0.0.0.0/0"] # Allowing traffic in from all sources
+  #   from_port   = 0             # Allowing any incoming port
+  #   to_port     = 0             # Allowing any outgoing port
+  #   protocol    = "-1"          # Allowing any outgoing protocol 
+  #   cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
+  # }
 
   egress {
     from_port   = 0             # Allowing any incoming port
