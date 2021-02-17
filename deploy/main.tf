d544f7471c5ce66c562c65cc8e9041ca69253df1
Joel Shin <joel@fluxxlabs.com>
2021-02-16 19:34:34 -0800

    rm comments

--- deploy/main.tf
+++ deploy/main.tf
@@ -175,11 +175,10 @@ resource "aws_alb" "weather_alb" {
     aws_default_subnet.default_subnet_b.id,
     aws_default_subnet.default_subnet_c.id
   ]
-  # Referencing the security group
+
   security_groups = [aws_security_group.load_balancer_security_group.id]
 }
 
-# Creating a security group for the load balancer:
 resource "aws_security_group" "load_balancer_security_group" {
   description = "allow access to application load balancer"
   name        = "weather-lb-sg"
@@ -191,24 +190,6 @@ resource "aws_security_group" "load_balancer_security_group" {
     to_port     = 80
     cidr_blocks = ["0.0.0.0/0"]
   }
-
-  # ingress {
-  #   # from_port   = 80 # Allowing traffic in from port 80
-  #   # to_port     = 80
-  #   # protocol    = "tcp"
-  #   # cidr_blocks = ["0.0.0.0/0"] # Allowing traffic in from all sources
-  #   from_port   = 0             # Allowing any incoming port
-  #   to_port     = 0             # Allowing any outgoing port
-  #   protocol    = "-1"          # Allowing any outgoing protocol 
-  #   cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
-  # }
-
-  # egress {
-  #   from_port   = 0             # Allowing any incoming port
-  #   to_port     = 0             # Allowing any outgoing port
-  #   protocol    = "-1"          # Allowing any outgoing protocol 
-  #   cidr_blocks = ["0.0.0.0/0"] # Allowing traffic out to all IP addresses
-  # }
 
   egress {
     from_port   = 3000
